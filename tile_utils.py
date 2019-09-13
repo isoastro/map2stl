ee402df288affa057f316320c8557f7e0fd94c36
Andrew Curtis <a.curtis.91@gmail.com>
2019-09-12 21:28:03 -0700

    Create classes for each level of data, with conversion functions between all

--- tile_utils.py
+++ tile_utils.py
@@ -3,8 +3,109 @@ import os.path
 import urllib.request
 import imageio
 import matplotlib.pyplot as plt
+from collections import namedtuple
 
 TILE_SIZE = 256
+WGS84_RADIUS = 6378137  # Meters
+WGS84_CIRCUMFERENCE = 2 * np.pi * WGS84_RADIUS
+TILE_RESOLUTION = WGS84_CIRCUMFERENCE / TILE_SIZE
+ORIGIN_METERS = WGS84_CIRCUMFERENCE / 2
+
+
+def _resolution(zoom):
+    """Calculate meters/pixel"""
+    return TILE_RESOLUTION / (2 ** zoom)
+
+
+class LatLon(namedtuple('LatLon', 'lat lon')):
+    def mercator(self):
+        """Convert latitude and longitude (degrees) in WGS84 to meters in Spherical Mercator"""
+        # https://en.wikipedia.org/wiki/Mercator_projection#Derivation_of_the_Mercator_projection
+
+        x = WGS84_RADIUS * (np.radians(self.lon))
+
+        lat = np.radians(self.lat)
+        y = WGS84_RADIUS * np.log(np.tan((np.pi / 4) + (lat / 2)))
+
+        return Mercator(x, y)
+
+    def pixel(self):
+        return self.mercator().pixel()
+
+    def tile(self):
+        return self.pixel().tile()
+
+
+class Mercator(namedtuple('Mercator', 'x y')):
+    def lat_lon(self):
+        """Convert meters in Spherical Mercator to latitude and longitude (degrees) in WGS84"""
+        # https://en.wikipedia.org/wiki/Mercator_projection#Derivation_of_the_Mercator_projection
+
+        lon = np.degrees(self.x / WGS84_RADIUS)
+
+        lat = 2 * np.arctan(np.exp(self.y / WGS84_RADIUS)) - (np.pi / 2)
+        lat = np.degrees(lat)
+
+        return LatLon(lat, lon)
+
+    def pixel(self, zoom):
+        """Convert meters in Spherical Mercator to a pixel location given a zoom level"""
+        r = _resolution(zoom)
+        x = (self.x + ORIGIN_METERS) / r
+        y = (self.y + ORIGIN_METERS) / r
+
+        return Pixel(x, y, zoom)
+
+    def tile(self):
+        return self.pixel().tile()
+
+
+class Pixel(namedtuple('Pixel', 'x y zoom')):
+    @property
+    def resolution(self):
+        """Calculate meters/pixel"""
+        return _resolution(self.zoom)
+
+    def mercator(self):
+        """Convert a pixel location to meters in Spherical Mercator"""
+        r = self.resolution
+
+        x = self.x * r - ORIGIN_METERS
+        y = self.y * r - ORIGIN_METERS
+
+        return Mercator(x, y)
+
+    def tile(self):
+        """Convert a pixel location to a tile x, y, and zoom. Zoom is not needed for calculation but needed for the tile
+            to be useful"""
+        x = (self.x / TILE_SIZE) - 1
+        y = (self.y / TILE_SIZE) - 1
+
+        return Tile(x, y, pixel.zoom)
+
+    def lat_lon(self):
+        return self.mercator().lat_lon()
+
+
+class Tile(namedtuple('Tile', 'x y zoom')):
+    @property
+    def resolution(self):
+        """Calculate meters/pixel"""
+        return _resolution(self.zoom)
+
+    def pixel(self):
+        """Convert a tile x, y, and zoom to a pixel location. Zoom is carried through to make the pixel meaningful"""
+        x = (tile.x + 1) * TILE_SIZE
+        y = (tile.y + 1) * TILE_SIZE
+
+        return Pixel(x, y, tile.zoom)
+
+    def mercator(self):
+        return self.pixel().mercator()
+
+    def lat_lon(self):
+        return self.mercator().lat_lon()
+
 
 def sec(x):
     return 1.0 / np.cos(x)
@@ -94,23 +195,50 @@ def build_grid(lat, lon, zoom):
 
 
 if __name__ == '__main__':
-    lat, lon = 46.852248, -121.757702  # Rainier
+    # lat, lon = 46.852248, -121.757702  # Rainier
+    lat = 85
+    lon = 54
+    latlon = LatLon(lat, lon)
+    print('Original lat and lon')
+    print(f'lat == {latlon.lat}')
+    print(f'lon == {latlon.lon}')
+    print()
+
+    merc = latlon.mercator()
+    print('Converted to mercator')
+    print(f'x == {merc.x}')
+    print(f'y == {merc.y}')
+    print()
+
     zoom = 11
+    pixel = merc.pixel(zoom)
+    print(f'Converted to pixel (zoom == {zoom})')
+    print(f'x == {pixel.x}')
+    print(f'y == {pixel.y}')
+    print(f'zoom == {pixel.zoom}')
+    print()
 
-    lat = np.linspace(lat - 0.5, lat + 0.5, 9)
-    lon = np.linspace(lon - 0.5, lon + 0.5, 5)
-    x, y = lat_lon_to_tile(lat, lon, zoom)
-    lat_derived, lon_derived = tile_to_lat_lon(x, y, zoom)
+    tile = pixel.tile()
+    print('Converted to tile')
+    print(f'x == {tile.x}')
+    print(f'y == {tile.y}')
+    print(f'zoom == {tile.zoom}')
+    print()
 
-    x_int = np.unique(x.astype(np.int))
-    y_int = np.unique(y.astype(np.int))
+    pixel2 = tile.pixel()
+    print('Converted back to pixel')
+    print(f'x == {pixel2.x}')
+    print(f'y == {pixel2.y}')
+    print(f'zoom == {pixel2.zoom}')
+    print()
 
-    grid = build_grid(lat, lon, zoom)
-    print(grid[1:10, 255])
-    print(grid[1:10, 256])
+    merc2 = pixel2.mercator()
+    print(f'Converted back to mercator')
+    print(f'x == {merc2.x}')
+    print(f'y == {merc2.y}')
+    print()
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.imshow(grid)
-    # fig.show()
-    plt.show()
+    latlon2 = merc2.lat_lon()
+    print(f'Converted back to lat and lon')
+    print(f'lat == {latlon2.lat}')
+    print(f'lon == {latlon2.lon}')
