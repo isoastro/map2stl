25699dab313437d48f5cb3187b68eed5a62b6418
Andrew Curtis <a.curtis.91@gmail.com>
2019-09-16 22:13:11 -0700

    Try to build a 2d array of meter positions. Super unoptimized

--- tile_utils.py
+++ tile_utils.py
@@ -4,6 +4,7 @@ import urllib.request
 import imageio
 import matplotlib.pyplot as plt
 from collections import namedtuple
+from dataclasses import dataclass
 
 TILE_SIZE = 256
 WGS84_RADIUS = 6378137  # Meters
@@ -13,7 +14,7 @@ ORIGIN_METERS = WGS84_CIRCUMFERENCE / 2
 
 
 def _resolution(zoom):
-    """Calculate meters/pixel"""
+    """Calculate meters/pixel (at equator)"""
     return TILE_RESOLUTION / (2 ** zoom)
 
 
@@ -95,10 +96,10 @@ class TileCoords(namedtuple('Tile', 'x y zoom')):
 
     def pixel(self):
         """Convert a tile x, y, and zoom to a pixel location. Zoom is carried through to make the pixel meaningful"""
-        x = (tile.x + 1) * TILE_SIZE
-        y = (tile.y + 1) * TILE_SIZE
+        x = (self.x + 1) * TILE_SIZE
+        y = (self.y + 1) * TILE_SIZE
 
-        return Pixel(x, y, tile.zoom)
+        return Pixel(x, y, self.zoom)
 
     def mercator(self):
         return self.pixel().mercator()
@@ -107,8 +108,25 @@ class TileCoords(namedtuple('Tile', 'x y zoom')):
         return self.mercator().lat_lon()
 
 
-def sec(x):
-    return 1.0 / np.cos(x)
+class Tile(TileCoords):
+    _data = None
+
+    # TODO: This is kind of dumb
+    @property
+    def data(self):
+        if self._data is None:
+            x = np.zeros((TILE_SIZE, TILE_SIZE))
+            y = np.zeros((TILE_SIZE, TILE_SIZE))
+            origin = self.pixel()
+            origin_m = origin.mercator()
+            for i in range(TILE_SIZE):
+                for j in range(TILE_SIZE):
+                    pixel = Pixel(origin.x + i, origin.y + j, origin.zoom)
+                    merc = pixel.mercator()
+                    x[i, j] = merc.x
+                    y[i, j] = merc.y
+            self._data = np.dstack((x - origin_m.x, y - origin_m.y))
+        return self._data
 
 
 def rgb_to_meters(arr):
@@ -119,56 +137,6 @@ def rgb_to_meters(arr):
     return (r * 256 + g + b / 256) - 32768
 
 
-def int_and_dec(x):
-    """Returns integer and decimal part of a number"""
-    i = int(x)
-    return i, float(x - i)
-
-
-def lat_lon_to_tile(lat, lon, zoom):
-    """Converts a given latitude and longitude (degrees) to a tile x, y"""
-    n = 2.0 ** zoom
-    x = n * ((lon + 180.0) / 360.0)
-    lat = np.radians(lat)
-    y = n * (1 - (np.log(np.tan(lat) + sec(lat)) / np.pi)) / 2.0
-    return x, y
-
-
-def pixels_to_meters(x, y, zoom):
-    res = 2 * np.pi * 6378137 / TILE_SIZE
-    res /= (2 ** zoom)
-    origin = 2 * np.pi * 6378137 / 2.0
-
-    mx = x * res - origin
-    my = y * res - origin
-
-
-def tile_to_lat_lon(x, y, zoom):
-    """Converts a given tile x, y to latitude and longitude (degrees)"""
-    n = 2.0 ** zoom
-    lon = x / n * 360.0 - 180.0
-    lat = np.degrees(np.arctan(np.sinh(np.pi * (1 - 2 * y / n))))
-    return lat, lon
-
-
-def tile_to_url(x, y, zoom):
-    url = f'https://s3.amazonaws.com/elevation-tiles-prod/terrarium/{zoom:d}/{x:d}/{y:d}.png'
-    return url
-
-
-def tile_to_filename(x, y, zoom, output_dir='tiles'):
-    filename = os.path.join(output_dir, f'{zoom:d}_{x:d}_{y:d}.png')
-    return filename
-
-
-def save_tile(x, y, zoom):
-    url = tile_to_url(x, y, zoom)
-    filename = tile_to_filename(x, y, zoom)
-    if not os.path.exists(filename):
-        urllib.request.urlretrieve(url, filename)
-        print(f'Saved {url} to {filename}')
-    return filename
-
 
 def get_grid_from_file(filename):
     arr = np.array(imageio.imread(filename))
@@ -176,22 +144,6 @@ def get_grid_from_file(filename):
     X, Y = np.meshgrid(x, x)
     Z = rgb_to_meters(arr)
     return np.dstack((X, Y, Z))
-
-
-def build_grid(lat, lon, zoom):
-    x, y = lat_lon_to_tile(lat, lon, zoom)
-    x = np.sort(np.unique(x.astype(np.int)))
-    y = np.sort(np.unique(y.astype(np.int)))
-    print(x, y)
-    grid = np.zeros((len(y) * TILE_SIZE, len(x) * TILE_SIZE, 3))
-    for i, yidx in enumerate(y):
-        for j, xidx in enumerate(x):
-            filename = save_tile(xidx, yidx, zoom)
-            subgrid = get_grid_from_file(filename)
-            subgrid[:, :, 0] += j * TILE_SIZE
-            subgrid[:, :, 1] += i * TILE_SIZE
-            print(subgrid.shape)
-    return grid
 
 
 if __name__ == '__main__':
@@ -244,3 +196,11 @@ if __name__ == '__main__':
                 res = fn(zoom)
                 print(f'{obj}.{meth}(zoom={zoom}) = {res}')
 
+    print()
+    print('Converting tile coordinates to meters')
+
+    tile = TileCoords(1330, 2043, 11)
+    origin = tile.pixel()
+
+    t = Tile(*tile)
+    print(t.data)
